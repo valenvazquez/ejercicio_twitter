768acf83aef70ddcffd3acfb7d86508751cdc70b
valenvazquez <valenvazquez21@gmail.com>
2021-10-12 11:41:27 -0300

    eliminar

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const publicRoutes = require("./publicRoutes");
-const adminRoutes = require("./adminRoutes");
-
-module.exports = (app) => {
-  app.use(publicRoutes);
-  app.use("/admin", adminRoutes);
-};
