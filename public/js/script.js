865fa9d9aa42d7a0ae0becad387b51ad1e586084
jonathanb88 <jonhybw@gmail.com>
2021-10-15 13:59:40 -0300

    Landing y modals completados 100%

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,15 @@
 $("#textSignIn").click(function () {
-  $("#registerPage").replaceWith("hola");
+  $("#registerPage").addClass("d-none");
+  $("#registerPage").removeClass("d-flex");
+  $("#loginpage").addClass("d-flex");
+  $("#loginpage").removeClass("d-none");
+});
+
+$("#textSignUp").click(function () {
+  $("#loginpage").addClass("d-none");
+  $("#loginpage").removeClass("d-flex");
+  $("#registerPage").addClass("d-flex");
+  $("#registerPage").addClass("flex-column");
+  $("#registerPage").addClass("justify-content-center");
+  $("#registerPage").removeClass("d-none");
 });
